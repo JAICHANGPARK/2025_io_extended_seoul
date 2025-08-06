ef0fdd21e8f29c62520eec89868fb2313ebb268b
Dreamwalker <aristojeff@gmail.com>
2025-08-06 21:21:43 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 
 import 'my_app.dart';
 
+final String geminiApiKey = "";
+final String huggingfaceKey = "";
+final String obsidianKey = "";
+final String modelName = "gemini-2.5-flash";
+
 void main() {
   runApp(const MyApp());
 }
